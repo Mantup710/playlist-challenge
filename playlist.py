5cd742c4035537b434960086d49b14fe18dad8af
Putnam Harris <putnamharrisiii@gmail.com>
2026-09-22 10:27:08 -0500

    Adding a song to the SONGS list

--- playlist.py
+++ playlist.py
@@ -4,6 +4,7 @@ SONGS = [
     ("Imagine", "John Lennon"),
     ("Blinding Lights", "The Weeknd"),
     ("Viva La Vida", "Coldplay"),
+    ("Is This It", "The Strokes")
 ]
 
 def print_playlist(songs):
